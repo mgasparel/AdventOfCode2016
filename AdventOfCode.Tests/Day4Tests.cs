ef5cd560de7c487cd4d354d42508e46916bcad03
Mike Gasparelli <magasparel@gmail.com>
2016-12-04 10:19:23 -0500

    Add Day 4 challenges

---
+++ AdventOfCode.Tests/Day4Tests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static AdventOfCode.AdventOfCodeDay4;
+using System.Linq;
+using System.Diagnostics;
+
+namespace AdventOfCode.Tests
+{
+
+    [TestClass]
+    public class Day4Tests
+    {
+        public static string Input;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Input = System.IO.File.ReadAllText("Input\\Day4.txt");
+        }
+
+        /*
+            Each room consists of an encrypted name (lowercase letters 
+            separated by dashes) followed by a dash, a sector ID, and 
+            a checksum in square brackets.
+
+            A room is real (not a decoy) if the checksum is the five 
+            most common letters in the encrypted name, in order, with 
+            ties broken by alphabetization. For example:
+
+            aaaaa-bbb-z-y-x-123[abxyz] is a real room because the 
+            most common letters are a (5), b (3), and then a tie 
+            between x, y, and z, which are listed alphabetically.
+        */
+        [TestMethod]
+        public void Day4Exercise1()
+        {
+            var adv = new AdventOfCodeDay4();
+            List<Room> rooms = adv.ParseRooms(Input);
+
+            var validSectorSum = rooms.Where(x => x.IsRealRoom).Sum(x => x.Sector);
+
+            Assert.AreEqual(158835, validSectorSum);
+        }
+
+        /*
+            To decrypt a room name, rotate each letter forward through 
+            the alphabet a number of times equal to the room's sector 
+            ID. A becomes B, B becomes C, Z becomes A, and so on. 
+            Dashes become spaces.
+
+            For example, the real name for qzmt-zixmtkozy-ivhz-343 
+            is very encrypted name.
+
+            What is the sector ID of the room where North Pole 
+            objects are stored?
+         */
+        [TestMethod]
+        public void Day4Exercise2()
+        {
+            var adv = new AdventOfCodeDay4();
+            List<Room> rooms = adv.ParseRooms(Input);
+
+            foreach(var room in rooms)
+            {
+                Debug.Print(room.DecryptedName);
+            }
+
+            var northPole = rooms
+                .Where(x => x.DecryptedName.Contains("north"))
+                .First();
+
+            Assert.AreEqual(993, northPole.Sector);
+        }
+
+        [TestMethod]
+        public void TestShiftCypher()
+        {
+            var room = new Room
+            {
+                EncryptedName = "nvrgfezqvu-avccpsvre-crsfirkfip",
+                Sector = 217,
+                Checksum = "abdfs"
+            };
+
+            Assert.AreEqual("weaponized jellybean laboratory", room.DecryptedName);
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCodeDay4.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class AdventOfCodeDay4
+    {
+        public AdventOfCodeDay4()
+        {
+        }
+
+        public List<Room> ParseRooms(string input)
+        {
+            var rooms = new List<Room>();
+
+            var inputLines = Common.GetLines(input);
+
+            foreach (var line in inputLines)
+            {
+                var matches = Regex.Matches(line, @"((?:[a-zA-Z]+(?:\-)*)+)(?:\-)(\d{3})\[([a-zA-Z]+)\]");
+
+                var room = new Room
+                {
+                    EncryptedName = matches[0].Groups[1].Value,
+                    Sector = int.Parse(matches[0].Groups[2].Value),
+                    Checksum = matches[0].Groups[3].Value
+                };
+
+                rooms.Add(room);
+            }
+
+            return rooms;
+        }
+
+        public class Room
+        {
+            public string EncryptedName { get; set; }
+            public int Sector { get; set; }
+            public string Checksum { get; set; }
+
+            public Room(string encryptedName, int sector, string checksum)
+            {
+                EncryptedName = encryptedName;
+                Sector = sector;
+                Checksum = checksum;
+            }
+
+            public Room()
+            {
+
+            }
+
+            public bool IsRealRoom
+            {
+                get
+                {
+                    return ChecksumCalculator.Calculate(EncryptedName) == Checksum;
+                }
+            }
+
+            public string DecryptedName
+            {
+                get
+                {
+                    return ShiftCypherDecryptor.Decrypt(EncryptedName, Sector);
+                }
+            }
+        }
+
+        public static class ChecksumCalculator
+        {
+            public static string Calculate(string input)
+            {
+                var checksum = input
+                    .Replace("-", "")
+                    .ToCharArray()
+                    .GroupBy(x => x)
+                    .Select(x => new { Char = x.Key, Count = x.Count() })
+                    .OrderByDescending(x => x.Count)
+                    .ThenBy(x => x.Char)
+                    .Take(5)
+                    .Select(x => x.Char)
+                    .ToList();
+
+                return string.Join("", checksum);
+            }
+        }
+
+        public static class ShiftCypherDecryptor
+        {
+            private static Int16 NumLettersInAlphabet = 26;
+
+            public static string Decrypt(string input, int shiftFactor)
+            {
+                string decrypted = "";
+
+                foreach (char letter in input.ToCharArray())
+                {
+                    char newChar;
+
+                    if (letter == '-')
+                    {
+                        newChar = ' ';
+                    }
+                    else
+                    {
+                        int originalAlphabetIndex = GetAlphabetIndex(letter);
+                        int shiftedAlphabetIndex = originalAlphabetIndex + shiftFactor;
+                        int wrappedShiftedAlphabetIndex = shiftedAlphabetIndex % NumLettersInAlphabet;
+
+                        newChar = GetCharAtAlphabetIndex(wrappedShiftedAlphabetIndex);
+                    }
+
+                    decrypted += newChar;
+                }
+
+                return decrypted;
+            }
+
+            private static int GetAlphabetIndex(char letter)
+            {
+                return letter - 'a' + 1;
+            }
+
+            private static char GetCharAtAlphabetIndex(int index)
+            {
+                return index == 0
+                    ? 'z'
+                    : (char)(index + 'a' - 1);
+            }
+        }
+    }
+}
